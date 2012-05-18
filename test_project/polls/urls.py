4df1b5a1ad3963ccea2dc59d0a8fb642917425a4
Bojan Mihelac <bmihelac@mihelac.org>
2012-05-18 22:26:47 +0200

    Update part of example app to CBV

--- test_project/polls/urls.py
+++ test_project/polls/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls.defaults import *
+from django.views.generic.list import ListView
+
 from models import Poll, Choice
+
 
 info_dict = {
     'queryset': Poll.objects.all(),
 }
 
+
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^$', ListView.as_view(model=Poll)),
     (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
     url(r'^(?P<object_id>\d+)/results/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='polls/results.html'), 'poll_results'),
     (r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
