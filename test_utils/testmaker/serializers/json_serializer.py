84f78c6ef2c40de25dbc8d0ed754ea1edbac56b4
Bojan Mihelac <bmihelac@mihelac.org>
2013-08-20 11:16:19 +0200

    Use json instead of simplejson
    json is available from Python 2.6

--- test_utils/testmaker/serializers/json_serializer.py
+++ test_utils/testmaker/serializers/json_serializer.py
@@ -1,5 +1,5 @@
 import base
-from django.utils import simplejson as json
+import json
 from test_utils.testmaker.serializers import REQUEST_UNIQUE_STRING, RESPONSE_UNIQUE_STRING
 
 class Serializer(base.Serializer):
